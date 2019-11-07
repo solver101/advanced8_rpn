da455012b0ac3df1195326f48af66a68a63ae410
solver101 <matt.kondamudi@gmail.com>
2019-11-06 22:53:36 -0500

    commit

---
+++ rpn.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import operator
+import colored
+from colored import stylize
+
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
+
+def main():
+    while True:
+        result = calculate(input("rpn calc> "))
+        print(stylize(result, colored.fg("red")))
+
+if __name__ == '__main__':
+    main()
+
